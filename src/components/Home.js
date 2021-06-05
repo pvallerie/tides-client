9b5f3e5c662ea54bca128adc2f91652dbceb35f8
pvallerie <petevallerie@gmail.com>
2021-06-05 11:47:33 -0400

    Add home component

---
+++ src/components/Home.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect, Fragment } from 'react'
+
+const Home = props => {
+    const { user } = props
+    const [location, setLocation] = useState(null)
+    const [highTide, setHighTide] = useState(null)
+    const [lowTide, setLowTide] = useState(null)
+
+    // when page loads
+        // get user's location
+        // get location lat/long
+        // get tides
+        // set tides states
+    
+    return (
+        <Fragment>
+            <p>{user.email}, you've made it home!</p>
+        </Fragment>
+    )
+}
+
+export default Home
